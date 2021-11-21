d7ba9935f0c67d4801064ac84d065367c8b30607
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-21 05:37:32 +0530

    ADDED : Welcome To Employee Payroll Service

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBService.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrollservice;
+
+public class EmployeePayrollDBService {
+    public static void main(String[] args) {
+        // Displaying Welcome message
+        System.out.println("Welcome to Employee Payroll Service");
+    }
+}
