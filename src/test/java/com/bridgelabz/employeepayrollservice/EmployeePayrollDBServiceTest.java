20fc3cc7fa861a870e2cfa870b1f52279a3c60f0
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-21 06:10:06 +0530

    ADDED : Retrieve data from database table

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBServiceTest.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.employeepayrollservice;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class EmployeePayrollDBServiceTest {
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollDBService employeePayrollDBService = new EmployeePayrollDBService();
+        List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.readData();
+        Assert.assertEquals(6, employeePayrollDataList.size());
+    }
 
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldnotMatchEmployeeCount() {
+        EmployeePayrollDBService employeePayrollDBService = new EmployeePayrollDBService();
+        List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.readData();
+        Assert.assertNotSame(4, employeePayrollDataList.size());
+    }
 }
